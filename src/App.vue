d965115a1fd1067496ba9f8e2503f777a3449877
youfeed <11247005@qq.com>
2024-01-24 23:28:08 +0800

    c

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>杰克布达</h1>
+    <section class="footer">
+      <a href="https://beian.miit.gov.cn/">
+        <p>皖ICP备 19004549-2号 公安备 34112402000344</p> 
+      </a>
+  </section>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
